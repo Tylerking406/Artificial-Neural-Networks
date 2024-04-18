3b1247cc1fa1acca158af7f7c1a0af68dcb8d556
Arinao Ndou <tylerking406@gmail.com>
2024-04-19 00:20:47 +0200

    I added Hyper Parameters for the model

--- classifier.py
+++ classifier.py
@@ -1,1 +1,18 @@
-# Main execution and implementation file for ANN
+
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import torchvision.transforms as transforms
+
+# Hyper Parameters
+inputSize = 784 # 24x24
+hiddenSize = 50 # try dif sizes
+num_classes = 100
+num_epochs =  36 # to be changed
+batch_size = 100
+lr = 0.001
+

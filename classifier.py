23348f3df5c8a12a6a305d8713a0a9dfdfb1984d
Arinao Ndou <tylerking406@gmail.com>
2024-04-19 01:31:10 +0200

    I loaded the MNIST data

--- classifier.py
+++ classifier.py
@@ -16,3 +16,16 @@ num_epochs =  36 # to be changed
 batch_size = 100
 lr = 0.001
 
+transform = transforms.Compose([transforms.ToTensor()])
+
+# Load MNIST from file
+DATA_DIR = "C:/Users/Student/OneDrive - University of Cape Town/Documents/2024/csc3022F/TUT/ML/TUT1"
+download_dataset = False
+
+train_mnist = datasets.MNIST(DATA_DIR, train=True, download=download_dataset, transform=transform)
+test_mnist = datasets.MNIST(DATA_DIR, train=False, download=download_dataset, transform=transform)
+
+# Shapes of train and test set
+print('train_mnist.shape: ',train_mnist.data.shape)
+print('test_mnist.shape: ',test_mnist.data.shape)
+
